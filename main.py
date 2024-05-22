26c0cf26b9c61fd20fb61c5d6c67fd7cc51a4ab3
iskra <iskrag@gmail.com>
2024-05-22 22:42:09 +0200

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = 'f00bba79d5ec4c03946caf6db33b998b'
 url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-04-22&sortBy=publis\
@@ -10,6 +11,16 @@ request = requests.get(url)
 # Get dictionary with data
 content = request.json()
 
+message = ''
+
 # Access the articles titles and description
 for article in content['articles']:
-    print(article['title'])
+    title = article['title']
+    description = article['description']
+    try:
+        message = message + title + '\n' + description + 2*'\n'
+    except TypeError:
+        continue
+message = message.encode('utf-8')
+send_email(message)
+
